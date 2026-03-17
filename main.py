5fbce87cea62261b8f520afd9b009fbd350ba646
Bullbagaren <julius.ramshage@bahnhof.se>
2026-03-17 13:19:41 +0100

    Added copyright notice

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+# Copyright (c) 2026 Julius Ramshage
+# All rights reserved.
+#
+# This source code is licensed under the MIT License found in the
+# LICENSE file in the root directory of this project.
 import sys
 import torch
 import tqdm
